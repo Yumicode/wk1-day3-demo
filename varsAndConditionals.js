ef014764061a905ab84eac832b7116dd70fa221d
Mayumi Park <mayumipark270@yahoo.com>
2022-12-19 00:49:48 -0700

    initial commit

---
+++ varsAndConditionals.js
@@ -0,0 +1,14 @@
+/*
+    In this file, we'll be using variables, conditionals,
+    and loops to play out a fight between Jon Snow and
+    Jamie Lannister.
+*/ 
+let obiwanAttack = 25
+let anakinAttack = 35
+
+if(obiwanAttack > anakinAttack){
+    console.log('Obiwan is stronger')
+}
+else{ 
+    console.log('Anakin has the power!')
+}
